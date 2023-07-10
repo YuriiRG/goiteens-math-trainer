1d1c7ad0f96657ed510dd6fc82af141006e6b9b2
YuriiRG <yuriirg@tutanota.com>
2023-07-10 16:25:04 +0200

    Implement all other calculation methods

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,9 @@ export default function App() {
           />
         </fieldset>
         {mode === "firstLast" && <FirstLastForm />}
+        {mode === "firstStep" && <FirstStepForm />}
+        {mode === "oneStep" && <OneStepForm />}
+        {mode === "two" && <TwoForm />}
       </main>
     </div>
   );
@@ -104,6 +107,239 @@ function FirstLastForm() {
       </button>
       {showResult && variables !== undefined && (
         <>Сума: {((variables.first + variables.last) / 2) * variables.n}</>
+      )}
+      {showResult && variables === undefined && "Невірні вхідні дані."}
+    </form>
+  );
+}
+
+const firstStepSchema = z.object({
+  n: z.string().regex(integerRegex).transform(integerTransform),
+  first: z.string().regex(floatRegex).transform(floatTransform),
+  step: z.string().regex(floatRegex).transform(floatTransform),
+});
+
+function FirstStepForm() {
+  const [inputVars, setInputVars] = useState({
+    n: "",
+    first: "",
+    step: "",
+  });
+  const [showResult, setShowResult] = useState(false);
+
+  const parseResult = firstStepSchema.safeParse(inputVars);
+  const variables = parseResult.success ? parseResult.data : undefined;
+
+  return (
+    <form className="flex flex-col items-start">
+      <NumberInput
+        id="n"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, n: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.n}
+        integer
+      />
+      <NumberInput
+        id="first"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, first: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.first}
+      />
+      <NumberInput
+        id="step"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, step: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.step}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setShowResult(true);
+        }}
+      >
+        Порахувати
+      </button>
+      {showResult && variables !== undefined && (
+        <>
+          Сума:{" "}
+          {((2 * variables.first + (variables.n - 1) * variables.step) / 2) *
+            variables.n}
+        </>
+      )}
+      {showResult && variables === undefined && "Невірні вхідні дані."}
+    </form>
+  );
+}
+
+const twoSchema = z.object({
+  n: z.string().regex(integerRegex).transform(integerTransform),
+  index: z.string().regex(integerRegex).transform(integerTransform),
+  value: z.string().regex(floatRegex).transform(floatTransform),
+  index2: z.string().regex(integerRegex).transform(integerTransform),
+  value2: z.string().regex(floatRegex).transform(floatTransform),
+});
+
+function TwoForm() {
+  const [inputVars, setInputVars] = useState({
+    n: "",
+    index: "",
+    value: "",
+    index2: "",
+    value2: "",
+    step: "",
+  });
+  const [showResult, setShowResult] = useState(false);
+
+  const parseResult = twoSchema.safeParse(inputVars);
+  const variables = parseResult.success ? parseResult.data : undefined;
+  const step = variables
+    ? (variables.value - variables.value2) /
+      (variables.index - variables.index2)
+    : undefined;
+  return (
+    <form className="flex flex-col items-start">
+      <NumberInput
+        id="n"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, n: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.n}
+        integer
+      />
+      <NumberInput
+        id="index"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, index: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.index}
+        integer
+      />
+      <NumberInput
+        id="value"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, value: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.value}
+      />
+      <NumberInput
+        id="index2"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, index2: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.index2}
+        integer
+      />
+      <NumberInput
+        id="value2"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, value2: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.value2}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setShowResult(true);
+        }}
+      >
+        Порахувати
+      </button>
+      {showResult && variables !== undefined && step !== undefined && (
+        <>
+          Сума:{" "}
+          {((2 * (variables.value - (variables.index - 1) * step) +
+            (variables.n - 1) * step) /
+            2) *
+            variables.n}
+        </>
+      )}
+      {showResult && variables === undefined && "Невірні вхідні дані."}
+    </form>
+  );
+}
+
+const oneStepSchema = z.object({
+  n: z.string().regex(integerRegex).transform(integerTransform),
+  index: z.string().regex(integerRegex).transform(integerTransform),
+  value: z.string().regex(floatRegex).transform(floatTransform),
+  step: z.string().regex(floatRegex).transform(floatTransform),
+});
+
+function OneStepForm() {
+  const [inputVars, setInputVars] = useState({
+    n: "",
+    index: "",
+    value: "",
+    step: "",
+  });
+  const [showResult, setShowResult] = useState(false);
+
+  const parseResult = oneStepSchema.safeParse(inputVars);
+  const variables = parseResult.success ? parseResult.data : undefined;
+
+  return (
+    <form className="flex flex-col items-start">
+      <NumberInput
+        id="n"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, n: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.n}
+        integer
+      />
+      <NumberInput
+        id="index"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, index: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.index}
+        integer
+      />
+      <NumberInput
+        id="value"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, value: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.value}
+      />
+      <NumberInput
+        id="step"
+        onChange={(e) => {
+          setInputVars({ ...inputVars, step: e.target.value });
+          setShowResult(false);
+        }}
+        value={inputVars.step}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setShowResult(true);
+        }}
+      >
+        Порахувати
+      </button>
+      {showResult && variables !== undefined && (
+        <>
+          Сума:{" "}
+          {((2 * (variables.value - (variables.index - 1) * variables.step) +
+            (variables.n - 1) * variables.step) /
+            2) *
+            variables.n}
+        </>
       )}
       {showResult && variables === undefined && "Невірні вхідні дані."}
     </form>
